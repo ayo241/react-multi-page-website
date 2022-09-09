07fb22f109f9e3a5e6fa395438f0cac748de0c3f
ayo241 <106593463+ayo241@users.noreply.github.com>
2022-09-09 13:14:39 +0100

    react-multi-page-website

---
+++ src/components/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Home(){
+  return (
+    <div className="home">
+      <div className="container">
+        <div className="row align-items-center my-5">
+        <div className="col-lg-7">
+          <img className="img-fluid rounded mb-4 mb-lg-0"
+            src=""
+            alt=""
+            />
+        </div>
+        <div className="col-lg-5">
+          <h1 className="font-weight-light">THIS IS OUR HOME PAGE</h1>
+          <p>Lorem IpsumLorem ipsum dolor sit amet, consectetur 
+          adipiscing elit, sed do eiusmod tempor incididunt ut 
+          labore et dolore magna aliqua. Ut enim ad minim veniam, 
+          quis nostrud exercitation ullamco laboris nisi ut aliquip 
+          ex ea commodo consequat. Duis aute irure dolor in 
+          reprehenderit in voluptate velit esse cillum dolore eu 
+          fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+          non proident, sunt in culpa qui officia deserunt mollit 
+          anim id est laborum.</p>
+        </div>
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Home;
